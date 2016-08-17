00cdfe1e73669b6e81d279a074ff4b6d41c5bf48
Divay Prakash <divayprakash3@gmail.com>
2016-08-17 07:54:21 +0530

    In progress

--- app/src/main/java/io/github/divayprakash/isprime/SplashActivity.java
+++ app/src/main/java/io/github/divayprakash/isprime/SplashActivity.java
@@ -7,13 +7,12 @@ import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
